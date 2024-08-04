2fb0a62a82ab7d85b4c863ec62eeb059015575e6
Amitesh Uniyal <amiteshuniyalz7@gmail.com>
2024-08-04 17:13:01 +0530

    first initial commit

---
+++ script/file.js
@@ -0,0 +1,9 @@
+let pop = document.getElementById("pop");
+
+      function openPop() {
+        pop.classList.add("open-pop");
+      }
+
+      function closePop() {
+        pop.classList.remove("open-pop");
+      }
